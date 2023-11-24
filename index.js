81e57cb9f0e5193c0f453c28fbe2296aa58d7571
kinwx <davimessias26@gmail.com>
2023-11-24 18:52:30 -0300

    qualquer coisa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { read_root } from './routes/read_root.js';
 import { get_items } from './routes/get_items.js';
 
 dotenv.config();
-
+// asdlnkasdnoikasd
 app.use(read_root);
 
 app.use(get_items);
